e7ab81f66ff50ecb567e84da17d271fa7a7716e8
aref <arefhukan@gmail.com>
2024-05-04 11:17:31 -0700

    its ok

--- src/helpers/onDragend.ts
+++ src/helpers/onDragend.ts
@@ -1,5 +1,3 @@
-import { Board } from "../data/Board";
-
 export const onDragEnd = (result: any, columns: any, setColumns: any) => {
   if (!result.destination) return;
   const { source, destination } = result;
@@ -25,19 +23,5 @@ export const onDragEnd = (result: any, columns: any, setColumns: any) => {
         items: destItems,
       },
     });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems,
-      },
-    });
-    setColumns(Board);
   }
 };
